78ae9cb6f0964984c63c160dff58fda903d15dbe
Denis Tola <denis.tola22@gmail.com>
2025-04-21 19:59:02 +0200

    Refactor send email

--- app/utils/email.py
+++ app/utils/email.py
@@ -1,25 +1,12 @@
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 import os
-from dotenv import load_env
+from dotenv import load_dotenv
 
-load_env()
+load_dotenv()
 
 SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 FROM_EMAIL = os.getenv("FROM_EMAIL")
 
 def send_reset_email(to_email: str, reset_link: str):
-    message = Mail(
-        from_email=FROM_EMAIL,
-        to_emails=to_email,
-        subject='Password Reset Request',
-        html_content=f"""
-        <p>Click the link below to reset your password. This link will expire soon:</p>
-        <a href="{reset_link}">Reset Password</a>
-        """
-    )
-    try:
-        sg = SendGridAPIClient(SENDGRID_API_KEY)
-        sg.send(message)
-    except Exception as e:
-        raise Exception("Failed to send reset email") from e
+    return 0
